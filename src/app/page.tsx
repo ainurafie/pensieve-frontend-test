b6bb13875bce308080eab44ed7a5d3920246e195
ainurafie <ainurafie@gmail.com>
2024-04-03 16:13:11 +0700

    list pokemon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,180 @@
-import Image from "next/image";
+'use client'
+import { useState, useEffect, useRef, useMemo } from 'react';
+
+type PokemonType = {
+  slot: number;
+  type: {
+    name: string;
+    url: string;
+  };
+};
+
+type Pokemon = {
+  name: string;
+  url: string;
+  types: PokemonType[];
+};
+
+type PokemonData = {
+  results: Pokemon[];
+};
+
+const typeColors: { [key: string]: string } = {
+  normal: 'bg-gray-400',
+  fighting: 'bg-red-600',
+  flying: 'bg-blue-400',
+  poison: 'bg-purple-600',
+  ground: 'bg-yellow-700',
+  rock: 'bg-gray-700',
+  bug: 'bg-green-500',
+  ghost: 'bg-indigo-500',
+  steel: 'bg-gray-600',
+  fire: 'bg-red-500',
+  water: 'bg-blue-500',
+  grass: 'bg-green-400',
+  electric: 'bg-yellow-400',
+  psychic: 'bg-purple-400',
+  ice: 'bg-blue-200',
+  dragon: 'bg-red-800',
+  dark: 'bg-gray-800',
+  fairy: 'bg-pink-400',
+};
+
+const SkeletonLoading = () => (
+  <div className="w-full flex gap-1 justify-center flex-row flex-wrap">
+    {[...Array(10)].map((_, index) => (
+      <div key={index} className="skeleton-loading placeholder w-32 h-32">
+      </div>
+    ))}
+  </div>
+);
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  // Inside the Home component
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  const [pokemonData, setPokemonData] = useState<PokemonData | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [offset, setOffset] = useState(0);
+  const limit = 20;
+  const observer = useRef<IntersectionObserver | null>(null);
+  const lastPokemonRef = useRef<HTMLDivElement | null>(null);
+  const [filterType, setFilterType] = useState<string | null>(null);
+  const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc'); // Track sort order
+  const [searchQuery, setSearchQuery] = useState<string>('');
+
+  const handleFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setFilterType(event.target.value);
+  };
+
+  const handleSortChange = () => {
+    setSortOrder((prevSortOrder) => (prevSortOrder === 'asc' ? 'desc' : 'asc'));
+  };
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
+      .then((response) => response.json())
+      .then((data: PokemonData) => {
+        const promises = data.results.map((pokemon) =>
+          fetch(pokemon.url).then((response) => response.json())
+        );
+        Promise.all(promises).then((pokemonDetails) => {
+          const newPokemonData = {
+            results: [...(pokemonData?.results || []), ...pokemonDetails.map((detail, index) => ({
+              ...data.results[index],
+              types: detail.types,
+            }))]
+          };
+          setPokemonData(newPokemonData);
+          setLoading(false);
+        });
+      });
+  }, [offset]);
+
+  useEffect(() => {
+    if (!pokemonData || !lastPokemonRef.current || loading) return;
+
+    observer.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        setLoading(true);
+        setOffset((prevOffset) => prevOffset + limit);
+      }
+    });
+
+    observer.current.observe(lastPokemonRef.current);
+
+    return () => {
+      if (observer.current) {
+        observer.current.disconnect();
+      }
+    };
+  }, [pokemonData, loading]);
+
+  const sortedPokemonData = useMemo(() => {
+    if (!pokemonData) return [];
+    return pokemonData.results.slice().sort((a, b) => {
+      const nameA = a.name.toUpperCase();
+      const nameB = b.name.toUpperCase();
+      if (sortOrder === 'asc') {
+        return nameA.localeCompare(nameB);
+      } else {
+        return nameB.localeCompare(nameA);
+      }
+    });
+  }, [pokemonData, sortOrder]);
+
+  return (
+    <main className='bg-purple-500 py-16'>
+      <div className='flex gap-3 items-center justify-end w-full px-16 mb-5'>
+        <button className='rounded-lg px-4 py-2 text-sm bg-gray-50 border-gray-300 text-gray-900 hover:bg-slate-100' onClick={handleSortChange}>
+          Name {sortOrder === 'asc' ? 'A/Z' : 'Z/A'}
+        </button>
+        <select id="countries" className='max-w-24 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' onChange={handleFilterChange} value={filterType || 'Semua Type'}>
+          <option disabled>Filter Type Pokemon</option>
+          {Object.keys(typeColors).map((type) => (
+            <option key={type}>{type}</option>
+          ))}
+        </select>
+        <input
+          type="text"
+          placeholder="Search Pokemon"
+          className='max-w-44 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
+          value={searchQuery}
+          onChange={handleSearchChange}
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {pokemonData && (
+        <div className='w-full flex gap-1 justify-center flex-row flex-wrap'>
+          {sortedPokemonData
+            .filter((pokemon) =>
+              (!filterType || pokemon.types.some((type) => type.type.name === filterType)) &&
+              (pokemon.name.toLowerCase().includes(searchQuery.toLowerCase()))
+            )
+            .map((pokemon, index) => (
+              <div key={index} ref={index === sortedPokemonData.length - 1 ? lastPokemonRef : null} className=''>
+                <div className="bg-white flex flex-col justify-end px-4">
+                  <div className='flex justify-center'>
+                    <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/')[6]}.png`} className="object-cover object-center" alt="" />
+                  </div>
+                  <h1 className='py-3 text-xs'>{index + 1}. {pokemon.name}</h1>
+                </div>
+                <div className='flex gap-0.5 items-center w-full'>
+                  {pokemon.types.map((type, index) => (
+                    <div key={index} className={`w-1/2 rounded-full py-0.5 ${typeColors[type.type.name]}`}>
+                      <p className='text-white text-xs text-center'>{type.type.name}</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+        </div>
+      )}
+      {loading && <SkeletonLoading />}
     </main>
   );
+
 }
